344842c7e6b4042d0b6370f4e38b4865605a6966
yeshrddy <yeshwanth2900@gmail.com>
2026-02-27 00:23:25 +0530

    dockerizing

--- backend/config/env.js
+++ backend/config/env.js
@@ -1,5 +1,5 @@
 const env = {
-	environment: process.env.NODE_ENV || 'development',
+	environment: process.env.ENV,
 	PORT: process.env.PORT || 5000,
 	MONGODB_URI: process.env.MONGODB_URI,
 	FRONTEND_URL: process.env.FRONTEND_URL,
